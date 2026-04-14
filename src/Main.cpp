522bac4e3324970073e83cab37d646d7c5f2729e
Mattia_Penatti <mattia.penatti1@campus.unimib.it>
2026-04-14 22:00:04 +0200

    updt

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,32 +3,10 @@
 #include <vector>
 #include "ReadAnchors.h"
 using namespace std;
-/// un estrapolamento dei punti dalle anchor(p.ti begin e end) da usare nello scorrimento della linee sweep
-/// per verificare se trattasi di punti begin o end.
-//funzione stampa chain
-//funzione lettura file
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) {
 
     vector <Anchor> anchors = readAnchors();
-
-    /*
-    #ifndef NDEBUG
-    cout << "\nStampo ancore lette:\n";
-    for (int i = 0; i < anchors.size(); i++) {
-        cout << i << ": (" 
-         << anchors[i].getXbegin() << "," << anchors[i].getYbegin() << ") -> ("
-         << anchors[i].getXend() << "," << anchors[i].getYend() << "), weight: "
-         << anchors[i].getWeight() << "\n";
-    } 
-    #endif
-    */
-
     solve(anchors);
     
 }
