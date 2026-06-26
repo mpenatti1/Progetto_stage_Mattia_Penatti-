8f2ab4841a467079f42eeb2511ccb689173087d8
Mattia_Penatti <mattia.penatti1@campus.unimib.it>
2026-06-26 18:36:26 +0200

    n

--- src/ChainSolver.cpp
+++ src/ChainSolver.cpp
@@ -152,10 +152,7 @@ void solve(std::vector<Anchor>& anchors){
         
         if(pti[i].isBegin){
             
-            /*#ifndef NDEBUG
-            cerr << "\nBEGIN"<< endl;
-            cerr << "Processing point: (" << pti[i].x << ", " << pti[i].y << ") - id: " << idcurr << endl;
-            #endif*/
+            
 
             #ifndef NDEBUG
             if (i >= n_pti * nextPercent / 100) {
@@ -176,26 +173,16 @@ void solve(std::vector<Anchor>& anchors){
                 
                 if (idPrec <0 || idPrec >= anchors.size() || idcurr < 0 || idcurr >= anchors.size()) {
                     cerr << "Error: idPrec or idcurr out of bounds. idPrec: " << idPrec << ", idcurr: " << idcurr << endl;
-                    continue; // Skip this iteration to avoid out-of-bounds access
+                    continue; 
                 }
 
                 Anchor &prev = anchors[idPrec];
                 anchors[idcurr].setPrec(idPrec);
                 
-                /*#ifndef NDEBUG
-                cerr << "Predecessor anchor id: " << idPrec << " with coordinates (" << prev.getXbegin() << ", " << prev.getYbegin() << ", " << prev.getXend() << ", " << prev.getYend() << ") and priority: " << kdpoints[idPrec]->getPriority() << endl;
-                #endif*/
-                //gapcost
-
-                //caso prima ancora fittizia
+                
                 if(idPrec == 0){
                     anchors[idcurr].setScore(0);
-                    /*#ifndef NDEBUG
-                    cerr << "prima ancora fittizia (come predecessore), non calcolo gap cost" << endl;
-                    cerr << "Best point found: (" << p->getX() << ", " << p->getY() << ") - id: " << idPrec << endl;
-                    cerr << "peso di "<< anchors[idcurr].getId() << " : " << anchors[idcurr].getWeight() << endl;
-                    cerr << "score di "<< anchors[idcurr].getId() << " : " << anchors[idcurr].getScore() << endl;
-                    #endif*/
+                    
                     continue;
                 }
                 if(idcurr == n_anchors-1){
@@ -205,18 +192,11 @@ void solve(std::vector<Anchor>& anchors){
 
                 int gap = ((anchors[idcurr].getXbegin()-anchors[idPrec].getXend())+(anchors[idcurr].getYbegin()-anchors[idPrec].getYend()));
                 
-                /*#ifndef NDEBUG
-                cerr << "Best point found: (" << p->getX() << ", " << p->getY() << ") - id: " << idPrec << endl;
-                cerr << "peso di "<< anchors[idcurr].getId() << " : " << anchors[idcurr].getWeight() << endl;
-                cerr << "score predecessore : " << prev.getScore() << endl;
-                cerr << "gap cost : " << gap << endl;
-                #endif*/
+               
 
                 anchors[idcurr].setScore(prev.getScore()-gap);
 
-                /*#ifndef NDEBUG
-                cerr << "Updated anchor score: " << anchors[idcurr].getScore() << endl;
-                #endif*/
+                
             }
             else {
                 #ifndef NDEBUG
@@ -231,12 +211,10 @@ void solve(std::vector<Anchor>& anchors){
 
             if (idcurr < 0 || idcurr >= anchors.size()) {
                 cerr << "Error: idcurr out of bounds. idcurr: " << idcurr << endl;
-                continue; // Skip this iteration to avoid out-of-bounds access
+                continue; 
             }
             if(idcurr==n_anchors-1){
-                /*#ifndef NDEBUG
-                cerr << "idcurr è l'ultima ancora fittizia, assegno stessa priorità della penultima ancora" << endl;
-                #endif*/
+                
                 kdpoints[idcurr]->setPriority(kdpoints[idPrec]->getPriority());
                 cerr << "priority : " << kdpoints[idcurr]->getPriority() << endl;
                 continue;
@@ -251,13 +229,7 @@ void solve(std::vector<Anchor>& anchors){
             else {
                 cerr << "errore: non esistono kdpoint o kdnodes per l'indice " << idcurr << endl;
             }
-            //////////
-            /*#ifndef NDEBUG
-            cerr << "\nEND"<< endl;
-            cerr << "Processing point: (" << pti[i].x << ", " << pti[i].y << ") - id: " << idcurr << endl;
-            cerr << "priority : " << kdpoints[idcurr]->getPriority() << endl;
             
-            #endif*/
             #ifdef USE_MAX_PRIORITY
             tree.updateMaxPriority(kdnodes[idcurr]);
             #endif
